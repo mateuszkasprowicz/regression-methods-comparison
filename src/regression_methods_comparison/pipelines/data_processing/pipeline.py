3d03b1ae547ea84e6807d6a9dd90a44363fb688d
Mateusz Kasprowicz <kasprowicz.mat@gmail.com>
2024-06-23 20:56:51 +0200

    Update data processing pipeline with new function names and parameters

--- src/regression_methods_comparison/pipelines/data_processing/pipeline.py
+++ src/regression_methods_comparison/pipelines/data_processing/pipeline.py
@@ -1,7 +1,7 @@
 from kedro.pipeline import Pipeline, node
 from kedro.pipeline.modular_pipeline import pipeline
 
-from .nodes import split_data, impute, encode
+from .nodes import split_data, impute_missing_values, encode_categories
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -14,12 +14,12 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs=["X_train", "X_test", "y_train", " y_test"],
             ),
             node(
-                func=impute,
+                func=impute_missing_values,
                 inputs=["X_train", "X_test", "y_train", " y_test", "params:override_me"],
                 outputs=["X_train", "X_test", "y_train", " y_test"],
             ),
             node(
-                func=encode,
+                func=encode_categories,
                 inputs=["X_train", "X_test", "y_train", " y_test", "params:override_me"],
                 outputs=["X_train", "X_test", "y_train", " y_test"],
             ),
@@ -29,21 +29,21 @@ def create_pipeline(**kwargs) -> Pipeline:
     abalone_pipeline = pipeline(
         pipe=processing_pipeline,
         inputs={"abalone"},
-        parameters={"params:override_me": "params:alpha"}, # TODO: change
+        parameters={"params:override_me": "params:abalone_processing"},
         namespace="abalone",
     )
 
     housing_pipeline = pipeline(
         pipe=processing_pipeline,
         inputs={"housing"},
-        parameters={"params:override_me": "params:alpha"}, # TODO: change
+        parameters={"params:override_me": "params:housing_processing"},
         namespace="housing",
     )
 
     wine_quality_pipeline = pipeline(
         pipe=processing_pipeline,
         inputs={"wine_quality"},
-        parameters={"params:override_me": "params:alpha"}, # TODO: change
+        parameters={"params:override_me": "params:wine_quality_processing"},
         namespace="wine_quality",
     )
 
