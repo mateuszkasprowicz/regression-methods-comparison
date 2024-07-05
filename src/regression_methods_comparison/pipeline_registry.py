e9549dfe7bfacce54d277619b3c3a0a9584921a7
Mateusz Kasprowicz <kasprowicz.mat@gmail.com>
2024-07-05 12:24:14 +0200

    Add parameters loading in pipeline registry code

--- src/regression_methods_comparison/pipeline_registry.py
+++ src/regression_methods_comparison/pipeline_registry.py
@@ -2,11 +2,20 @@
 
 from typing import Dict
 
-from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
+from kedro.config import OmegaConfigLoader
+from kedro.framework.project import settings
 
 from regression_methods_comparison.pipelines import data_processing as dp
 from regression_methods_comparison.pipelines import ml
+
+
+CONF_PATH = str(settings.CONF_SOURCE)
+CONF_LOADER = OmegaConfigLoader(conf_source=CONF_PATH)
+PARAMETERS = CONF_LOADER["parameters"]
+
+DATASETS = PARAMETERS["datasets"]
+MODEL_TYPES = PARAMETERS["models"]
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -15,10 +24,10 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
-    data_processing_pipeline = dp.create_pipeline()
-    modeling_pipeline = ml.create_pipeline()
+    data_processing_pipeline = dp.create_pipeline(datasets=DATASETS)
+    ml_pipeline = ml.create_pipeline(datasets=DATASETS, model_types=MODEL_TYPES)
     return {
-        "__default__": (data_processing_pipeline + modeling_pipeline),
-        "Data processing": data_processing_pipeline,
-        "Modeling": modeling_pipeline,
+        "__default__": (data_processing_pipeline + ml_pipeline),
+        "data_processing": data_processing_pipeline,
+        "ml": ml_pipeline,
     }
